7093de7c74478aca7162380a620d51fa99a83c74
Prathamesh Giri <97845082+prathameshgiri@users.noreply.github.com>
2024-02-16 16:25:34 +0530

    Add files via upload

--- footer/footer_html.js
+++ footer/footer_html.js
@@ -2,37 +2,37 @@ document.write(`
     <footer class="footer">
       <div class="footer_row">
         <div class="footer_col">
-          <h4 class="footer_col_heading">Info</h4>
+          <h4 class="footer_col_heading">About Us</h4>
           <ul class="footer_col_list_parent">
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">About Us</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Compressions</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Customers</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Service</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Collection</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#about">About Us</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://wa.me/918698333062">Contact</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#review">Customer</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshgiri.github.io/startechies/">Service</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#services">Collection</a></li>
           </ul>
         </div>
 
         <div class="footer_col">
           <h4 class="footer_col_heading">Explore</h4>
           <ul class="footer_col_list_parent">
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Free Designs</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Latest Designs</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Themes</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Popular Designs</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Art Skills</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">New Uploads</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshsgiri.github.io/prathamesh/">Website</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Image Gallery</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Work</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Personal Info</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Up-comming</a></li>
+            
           </ul>
         </div>
 
         <div class="footer_col">
-          <h4 class="footer_col_heading">Legal</h4>
+          <h4 class="footer_col_heading">Team</h4>
           <ul class="footer_col_list_parent">
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Customer Agreement</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Privacy Policy</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">GDPR</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Security</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Testimonials</a></li>
-            <li class="footer_col_lists"><a class="footer_col_links" href="#">Media Kit</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshgiri.github.io/portfolio/">Prathamesh Giri</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="https://alokbirge12.github.io/portfolio/">Alok Birge</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Sushant Biradar</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Rahul Dadge</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Adarsh Bondge</a></li>
+          
           </ul>
         </div>
 
