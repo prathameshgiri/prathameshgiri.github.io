7e4715d22a1e9ed8d1bac08393c8c225b236c754
Prathamesh Giri <97845082+prathameshgiri@users.noreply.github.com>
2024-02-16 15:45:23 +0530

    Add files via upload

---
+++ footer/footer_html.js
@@ -0,0 +1,59 @@
+document.write(`
+    <footer class="footer">
+      <div class="footer_row">
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Info</h4>
+          <ul class="footer_col_list_parent">
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">About Us</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Compressions</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Customers</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Service</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Collection</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Explore</h4>
+          <ul class="footer_col_list_parent">
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Free Designs</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Latest Designs</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Themes</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Popular Designs</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Art Skills</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">New Uploads</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Legal</h4>
+          <ul class="footer_col_list_parent">
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Customer Agreement</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Privacy Policy</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">GDPR</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Security</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Testimonials</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Media Kit</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Newsletter</h4>
+          <p>
+            Subscribe to our newsletter for a weekly dose
+            of news, updates, helpful tips, and
+            exclusive offers.
+          </p>
+          <form action="#">
+            <input type="text" placeholder="Your email" required>
+            <button type="submit">SUBSCRIBE</button>
+          </form>
+          <div class="icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+`);
