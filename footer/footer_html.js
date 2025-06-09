89337ec3a72e9f5879f7e8aed89730e426a3ced8
Prathamesh Giri <prathameshgiri.in@gmail.com>
2025-06-09 12:41:42 +0530

    Add files via upload

---
+++ footer/footer_html.js
@@ -0,0 +1,59 @@
+document.write(`
+    <footer class="footer">
+      <div class="footer_row">
+        <div class="footer_col">
+          <h4 class="footer_col_heading">About Us</h4>
+          <ul class="footer_col_list_parent">
+            <li class="footer_col_lists"><a class="footer_col_links" href="#about">About Us</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://wa.me/918698333062">Contact</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#review">Customer</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshgiri.github.io/startechies/">Service</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#services">Collection</a></li>
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Explore</h4>
+          <ul class="footer_col_list_parent">
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshsgiri.github.io">Website</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshgiri.github.io/gallery/">Image Gallery</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="https://github.com/prathameshgiri">Projects</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Personal Info</a></li>
+            <li class="footer_col_lists"><a class="footer_col_links" href="#">Up-comming</a></li>
+            
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Team</h4>
+          <ul class="footer_col_list_parent">
+          <li class="footer_col_lists"><a class="footer_col_links" href="https://prathameshgiri.github.io/portfolio/">Prathamesh Giri</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="https://alokbirge12.github.io/">Alok Birge</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Sushant Biradar</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Rahul Dadge</a></li>
+          <li class="footer_col_lists"><a class="footer_col_links" href="#">Adarsh Bondge</a></li>
+          
+          </ul>
+        </div>
+
+        <div class="footer_col">
+          <h4 class="footer_col_heading">Newsletter</h4>
+          <p>
+            Subscribe to our newsletter for a weekly dose
+            of news, updates, helpful tips, and
+            exclusive offers.
+          </p>
+          <form action="#">
+            <input type="text" placeholder="Your email" required>
+            <button type="submit">SUBSCRIBE</button>
+          </form>
+          <div class="icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-github"></i>
+          </div>
+        </div>
+      </div>
+    </footer>
+`);
